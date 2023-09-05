6426a2b3203cc0c674f8942be109d7687475d53c
Saurabh <saurabhsalunke91@gmail.com>
2023-09-05 19:47:01 +0530

    .env file ignored

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import HomePage from './pages/HomePage';
-import 'react-loading-skeleton/dist/skeleton.css'
-import { SkeletonTheme } from 'react-loading-skeleton';
 function App() {
   return (
-    <SkeletonTheme baseColor='#525151' highlightColor='#d0d0d0'>
       <HomePage/>
-    </SkeletonTheme>
   )
 }
 
